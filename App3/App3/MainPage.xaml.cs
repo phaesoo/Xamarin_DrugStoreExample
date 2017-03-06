3872f15485b1b6b1ca7b0621d476276230944bbb
Phaesoo <phaesoo@gmail.com>
2017-03-06 23:40:45 +0900

    Complete example

---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            bool isClickHospital = false;
+
+            SearchButtonHospital.Clicked += async (sender, e) =>
+            {
+                if (!isClickHospital)
+                {
+                    await SearchBtnsTop.TranslateTo(0, 200, 700, Easing.SinIn);
+                    isClickHospital = true;
+                }
+                else
+                {
+                    await SearchBtnsTop.TranslateTo(0, -1, 700, Easing.SinIn);
+                    isClickHospital = false;
+                }
+            };
+
+            bool isClickDrugStore = false;
+
+            SearchBtnDrugStore.Clicked += async (sender, e) =>
+            {
+                if (!isClickDrugStore)
+                {
+                    await SearchBtnBottom.TranslateTo(0, -200, 700, Easing.SinIn);
+                    isClickDrugStore = true;
+                }
+                else
+                {
+                    await SearchBtnBottom.TranslateTo(0, 1, 700, Easing.SinIn);
+                    isClickDrugStore = false;
+                }
+            };
+        }
+    }
+}
